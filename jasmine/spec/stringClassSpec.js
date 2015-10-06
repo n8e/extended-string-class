edc1913af9436359db086af99b2b867f095970fa
andela-nmartin <godmetweenciati@gmail.com>
2015-10-06 14:08:28 +0300

    String class methods
    - [Chore] added hasVowels and isQuestion
    - [Chore] added a readme file
    - [Chore] added upperCase and lowerCase
    - [Chore] added tests for the currency methods
    - [Chore] commented out the src file
    - [Chore] changed the expectation for ucFirst
    - [Chore] modified the ucFirst function
    - [Chore] refactored the src and test scripts
    - [Chore] modified comments
    - [Fixes] refactored regex matches for src
    - [Chore] modified isQuestion function
    - [Chore] adjusted js prettify configurations

--- jasmine/spec/stringClassSpec.js
+++ jasmine/spec/stringClassSpec.js
@@ -1,69 +1,111 @@
 // hasVowels(typeofBoolean​): Returns true if the string contains vowels. This method must implement Regular Expression.
-// toUpper(typeofString​): Returns the String in question but with all characters in upper cases as applicable.
-// toLower(typeofString​): Returns the String in question but with all characters in their lower cases as applicable.
-// ucFirst(typeofString​): Returns the String in question but changes the First Character to an Upper case.
-// isQuestion(typeofBoolean​): Return true if the string is a question (ending with a question mark). This method must implement Regular Expression.
-// words(typeofObject​,instanceofArray​): Returns a list of the words in the string, as an Array. This method must implement Regular Expression.
-// wordCount(typeofNumber​): Returns the number of words in the string. It must make use of the ​words​method above.
-// toCurrency(typeofString​): Returns a currency representation of the String e.g 11111.11should be represented as 11,111.11.
-// fromCurrency(typeofNumber​): Returns a number representation of the Currency String e.g 11,111.11should return 11111.11
-
-
-// All the prototyped methods are to be covered fully with tests
-// The tests should cover the methods as well as the conditions/procedures that the methods employ
-// All the comparators used for the tests must be the optimum or most efficient comparator for the scenario
-
-describe("Has Vowels", function(){
-  var chopStory = 'Serve me some chops!';
-  it("should return true if the string contains vowels.", function(){
+describe("Has Vowels", function() {  
+  it("should return true if the string contains vowels.", function() {
+    var chopStory = 'Serve me some chops!';
     expect(chopStory.hasVowels()).toBeTruthy();
+  });
+
+  it("should return true if the string contains vowels.", function() {
+    expect(('aeiou').hasVowels()).toBeTruthy();
+  });
+
+  it("should return true if the string contains vowels.", function() {
+    expect(('qwrtypsdfghjkl').hasVowels()).toBeFalsy();
   });
 });
 
-describe("To Upper", function(){
-  it("should return the String in question but with all characters in upper cases as applicable.", function(){
+// toUpper(typeofString​): Returns the String in question but with all characters in upper cases as applicable.
+describe("To Upper", function() {
+  it("should return the String in question but with all characters in upper cases as applicable.", function() {
     expect(('prime leads the autobots').toUpper()).toBe('PRIME LEADS THE AUTOBOTS');
   });
 });
 
-describe("To Lower", function(){
-  it("should return the String in question but with all characters in their lower cases as applicable.", function(){
+// toLower(typeofString​): Returns the String in question but with all characters in their lower cases as applicable.
+describe("To Lower", function() {
+  it("should return the String in question but with all characters in their lower cases as applicable.", function() {
     expect(('mIcKeY mOuSe').toLower()).toBe('mickey mouse');
   });
 });
 
-describe("Upper Case First", function(){
-  it("should return the String in question but changes the First Character to an Upper case.", function(){
-    expect(('justify your existence').ucFirst()).toBe('Justify Your Existence');
+// ucFirst(typeofString​): Returns the String in question but changes the First Character to an Upper case.
+describe("Upper Case First", function() {
+  it("should return the String in question but changes the First Character to an Upper case.", function() {
+    expect(('justify your existence').ucFirst()).toBe('Justify your existence');
   });
 });
 
-describe("Is Question", function(){
-  it("should return true if the string is a question (ending with a question mark).", function(){
+// isQuestion(typeofBoolean​): Return true if the string is a question (ending with a question mark). This method must implement Regular Expression.
+describe("Is Question", function() {
+  it("should return true if the string is a question (ending with a question mark).", function() {
     expect(('Could this be the end of time?').isQuestion()).toBeTruthy();
   });
+
+  it("should return true if the string is a question (ending with a question mark).", function() {
+    expect(('What? He cannot have escaped!').isQuestion()).toBeFalsy();
+  });
 });
 
-describe("Words", function(){
-  it("should return a list of the words in the string, as an Array.", function(){
+// words(typeofObject​,instanceofArray​): Returns a list of the words in the string, as an Array. This method must implement Regular Expression.
+describe("Words", function() {
+  it("should return a list of the words in the string, as an Array.", function() {
     expect(('find your courage').words()).toEqual(['find', 'your', 'courage']);
   });
+
+  it("should return a list of the words in the string, as an Array.", function() {
+    expect(('find your courage !@#$%^').words()).toEqual(['find', 'your', 'courage']);
+  });
 });
 
-describe("Word Count", function(){
-  it("should return the number of words in the string. It must make use of the ​words​ method.", function(){
+// wordCount(typeofNumber​): Returns the number of words in the string. It must make use of the ​words​method above.
+describe("Word Count", function() {
+  it("should return the number of words in the string. It must make use of the ​words​ method.", function() {
     expect(('find your courage').wordCount()).toEqual(3);
   });
-});
 
-describe("To Currency", function(){
-  it("should return a currency representation of the String e.g 11111.11 should be represented as 11,111.11.", function(){
-    expect(('11111.11').toCurrency()).toEqual('11,111.11');
+  it("should return the number of words in the string. It must make use of the ​words​ method.", function() {
+    expect(('find your courage !@#$%^').wordCount()).toEqual(3);
   });
 });
 
-describe("From Currency", function(){
-  it("should return a number representation of the Currency String e.g 11,111.11should return 11111.11", function(){
-    expect(('11,111.11').fromCurrency()).toEqual('11111.11');
+// toCurrency(typeofString​): Returns a currency representation of the String e.g 11111.11should be represented as 11,111.11.
+describe("To Currency", function() {
+  it("should return a currency representation of the String e.g 11111.11 should be represented as 11,111.11.", function() {
+    expect(('11111.11').toCurrency()).toEqual('11,111.11');
+  });
+
+  it("should return a currency representation of the String e.g 11111.11111 should be represented as 1,111.11111.", function() {
+    expect(('11111.11111').toCurrency()).toEqual('11,111.11111');
+  });
+
+  it("should return a currency representation of the String e.g 11111 should return 11,111", function() {
+    expect(('11111').toCurrency()).toEqual('11,111');
+  });
+
+  it("should return a currency representation of the String e.g 100000000 should return 100,000,000", function() {
+    expect(('100000000').toCurrency()).toEqual('100,000,000');
+  });
+});
+
+// fromCurrency(typeofNumber​): Returns a number representation of the Currency String e.g 11,111.11should return 11111.11
+describe("From Currency", function() {
+  it("should return a number representation of the Currency String e.g 11,111.11 should return 11111.11", function() {
+    expect(('11,111.11').fromCurrency()).toEqual(11111.11);
+  });
+
+  it("should return a number representation of the Currency String e.g 11111.11 should return 11111.11", function() {
+    expect(('11111.1111').fromCurrency()).toEqual(11111.1111);
+  });
+
+  it("should return a number representation of the Currency String e.g 11,111 should return 11111", function() {
+    expect(('11,111').fromCurrency()).toEqual(11111);
+  });
+
+  it("should return a number representation of the Currency String e.g 11111 should return 11111", function() {
+    expect(('11111').fromCurrency()).toEqual(11111);
+  });
+
+  it("should return a number representation of the Currency String e.g 100,000,000 should return 100000000", function() {
+    expect(('100,000,000').fromCurrency()).toEqual(100000000);
   });
 });
--- jasmine/src/stringClass.js
+++ jasmine/src/stringClass.js
@@ -1,53 +1,117 @@
-// All the methods should be prototyped into the default data type classes, and called from within (String, Number Classes). No functions should be declared outside the prototyping of the classes.
-// The methods that have the Regular Expression implementation in their descriptions, must be implemented with Regular Expressions.
-// All tests must tests all methods, including the conditionals that they may contain (Arguments are customized, in the tests, so all conditions in the method get tested)
-
-// All the methods are implemented with Regular Expression.
-// Code base is efficient, not redundant loops or method calls that could deter performance.
-
 // hasVowels(typeofBoolean​): Returns true if the string contains vowels. This method must implement Regular Expression.
 String.prototype.hasVowels = function() {
-  if(/[aeiou]/.test(this)) {
-    return true;
-  } else {
-    return false;
-  }
+  //to check any of the vowel characters within the []
+  return /[aeiou]/.test(this);
 };
 // toUpper(typeofString​): Returns the String in question but with all characters in upper cases as applicable.
 String.prototype.toUpper = function() {
-  if (/[a-z]/g) {
-      return /[A-Z]/;
+  var newStr = '';
+  // loop through the array of characters
+  for (var i = 0, n = this.length; i < n; i++) {
+    // condition to check for lowercase
+    // get the ascii code of every character 
+    if (this.charCodeAt(i) >= 97 && /[a-z]/g && this.charCodeAt(i) <= 122) {
+      // subtract 32 to make it uppercase
+      var upChar = this.charCodeAt(i) - 32;
+      newStr += String.fromCharCode(upChar);
+    } else {
+      newStr += this[i];
+    }
   }
+  return newStr;
 };
 // toLower(typeofString​): Returns the String in question but with all characters in their lower cases as applicable.
 String.prototype.toLower = function() {
-
+  var newStr = '';
+  // loop through the array of characters
+  for (var i = 0, n = this.length; i < n; i++) {
+    // condition to check for lowercase
+    if (this.charCodeAt(i) >= 65 && this.charCodeAt(i) <= 90) {
+      // get the ascii code of every character and add 32 to make it lowercase
+      var lowChar = this.charCodeAt(i) + 32;
+      newStr += String.fromCharCode(lowChar);
+    } else {
+      newStr += this[i];
+    }
+  }
+  return newStr;
 };
 // ucFirst(typeofString​): Returns the String in question but changes the First Character to an Upper case.
 String.prototype.ucFirst = function() {
-
+  var newStr = '';
+  // loop through the array of characters
+  for (var i = 0, n = this.length; i < n; i++) {
+    // condition to check for lowercase
+    // get the ascii code of every character and subtract 32 to make it uppercase
+    if (i === 0 && this.charCodeAt(i) >= 97 && this.charCodeAt(i) <= 122) {
+      var upChar = this.charCodeAt(i) - 32;
+      newStr += String.fromCharCode(upChar);
+    } else {
+      newStr += this[i];
+    }
+  }
+  return newStr;
 };
 // isQuestion(typeofBoolean​): Return true if the string is a question (ending with a question mark). This method must implement Regular Expression.
 String.prototype.isQuestion = function() {
-  if(/[?]/.test(this)) {
-    return true;
-  } else {
-    return false;
-  }
+  // checks whether there is a questionmark at the end of the string.
+  return /[\?]+?$/.test(this);
 };
 // words(typeofObject​,instanceofArray​): Returns a list of the words in the string, as an Array. This method must implement Regular Expression.
 String.prototype.words = function() {
-
+  // splits the string where there are white spaces throughout the string
+  return this.match(/\w+/gi);
 };
 // wordCount(typeofNumber​): Returns the number of words in the string. It must make use of the ​words​method above.
 String.prototype.wordCount = function() {
-
+  return this.words().length;
 };
 // toCurrency(typeofString​): Returns a currency representation of the String e.g 11111.11should be represented as 11,111.11.
 String.prototype.toCurrency = function() {
+  var number = this,
+    integer,
+    isNumber = /^[+-]?[\d]{1,3}(?:,?[\d]{3})*\.[\d]{2}/.test(number) || /^[+-]?[\d]{1,3}(?:,?[\d]{3})/.test(number);
 
+  if (isNumber) {
+    var decimalPart = '',
+      currencyString = '';
+    integer = number;
+    var twoPartNumber = number.split(/\./);
+    if (twoPartNumber.length === 2) {
+      // store the whole part
+      integer = twoPartNumber[0];
+      // store the fraction
+      decimalPart = twoPartNumber[1];
+    } else if (twoPartNumber.length === 1) {
+      // if there is only a whole part in the given number string
+      integer = twoPartNumber[0];
+    }
+    for (var i = integer.length - 1, cluster = 0; i >= 0; i--) {
+      // cluster is a counter for grouping sets of three digits
+      if (cluster % 3 === 0 && cluster !== 0) {
+        currencyString += ',';
+      }
+      currencyString += integer[i];
+      cluster++;
+    }
+    // join the whole part and the fraction after reversing the string
+    currencyString = currencyString.split('').reverse().join('');
+    // if the decimal part is not empty
+    if (decimalPart !== '') {
+      return [currencyString, '.', decimalPart].join('');
+    }
+    return currencyString;
+  }
+  return number;
 };
+
 // fromCurrency(typeofNumber​): Returns a number representation of the Currency String e.g 11,111.11should return 11111.11
 String.prototype.fromCurrency = function() {
+  var number = this,
+    isNumber = /[\d{1,3}(\,)?]+(\.[\d]+)?$/.test(number);
 
+  if (isNumber) {
+    return parseFloat(String(number.replace(/\,/g, '')));
+  }
+  return number;
 };
